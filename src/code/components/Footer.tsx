b7c03b9c78ef7bee20c1f8b6c88da1dfb4d30524
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-31 20:10:59 +0200

    Simplifie footer
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/components/Footer.tsx
+++ src/code/components/Footer.tsx
@@ -5,23 +5,9 @@ export default class App extends React.Component {
 	public render(): JSX.Element {
 		return (
 			<footer className="Footer">
-				<span>
-					<a href="https://github.com/SimonBrandner/personal-website">Source code on GitHub</a>
-				</span>
-				&nbsp;
-				&middot;
-				&nbsp;
-				<span>
-					Uses
-					&nbsp;
-					<a 
-						href="https://github.com/google/material-design-icons"
-						rel="noreferrer noopener"
-						target="_blank"
-					>
-						Material design icons
-					</a>
-				</span>
+				<a href="https://github.com/SimonBrandner/personal-website">Source code on GitHub</a>
+				&nbsp;&middot;
+				Uses&nbsp;<a href="https://github.com/google/material-design-icons">Material design icons</a>
 			</footer>
 		);
 	}
