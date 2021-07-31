cd80e7a7c3bce43cbbab19f93eebdea8346d459d
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-31 17:04:57 +0200

    Improve the contacts page
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/views/Contact.tsx
+++ src/code/views/Contact.tsx
@@ -7,11 +7,36 @@ export default class Contact extends React.Component {
 		return (
 			<View className="Contact">
 				<React.Fragment>
-					Email: simon.bra.ag@gmail.com <br />
-					Matrix: @simon.brandner:envs.net <br />
-					Twitter: @BrandnerSimon <br />
-					GitHub: @SimonBrandner <br />
-					Location: Milky Way, Earth, Czech Republic <br />
+					<a 
+						className="Contact_link Contact_link_email"
+						href="mailto:simon.bra.ag@gmail.com"
+					> 
+						Email
+					</a>
+					<a 
+						className="Contact_link Contact_link_twitter"
+						href="https://twitter.com/BrandnerSimon"
+					> 
+						Twitter
+					</a>
+					<a 
+						className="Contact_link Contact_link_matrix"
+						href="https://matrix.to/#/@simon.brandner:envs.net"
+					> 
+						Matrix
+					</a>
+					<a 
+						className="Contact_link Contact_link_github"
+						href="https://github.com/SimonBrandner"
+					> 
+						GitHub
+					</a>
+					<a 
+						className="Contact_link Contact_link_location"
+						href="https://www.google.com/maps/place/Czechia/"
+					> 
+						Location
+					</a>
 				</React.Fragment>
 			</View>
 		);
