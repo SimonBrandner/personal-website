c3cca4cdde14a9cf96dc0f08114eb7a5f4329905
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-31 19:51:31 +0200

    Reorder contacts
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/views/Contact.tsx
+++ src/code/views/Contact.tsx
@@ -8,28 +8,28 @@ export default class Contact extends React.Component {
 			<View className="Contact">
 				<React.Fragment>
 					<a 
-						className="Contact_link Contact_link_email"
-						href="mailto:simon.bra.ag@gmail.com"
-					> 
-						Email
-					</a>
-					<a 
-						className="Contact_link Contact_link_twitter"
-						href="https://twitter.com/BrandnerSimon"
-					> 
-						Twitter
-					</a>
-					<a 
 						className="Contact_link Contact_link_matrix"
 						href="https://matrix.to/#/@simon.brandner:envs.net"
 					> 
 						Matrix
 					</a>
 					<a 
+						className="Contact_link Contact_link_email"
+						href="mailto:simon.bra.ag@gmail.com"
+					> 
+						Email
+					</a>
+					<a 
 						className="Contact_link Contact_link_github"
 						href="https://github.com/SimonBrandner"
 					> 
 						GitHub
+					</a>
+					<a 
+						className="Contact_link Contact_link_twitter"
+						href="https://twitter.com/BrandnerSimon"
+					> 
+						Twitter
 					</a>
 					<a 
 						className="Contact_link Contact_link_location"
