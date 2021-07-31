f0bd89643800e2de81fa633aafecdd69d9cf738b
Šimon Brandner <simon.bra.ag@gmail.com>
2021-07-31 19:48:02 +0200

    Update copy
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/views/Home.tsx
+++ src/code/views/Home.tsx
@@ -8,7 +8,7 @@ export default class Home extends React.Component {
 			<View className="Home">
 				<React.Fragment>
 					Hello, my name is Šimon Brander, <br />
-					I am a student and a hobbyist programmer in my free time
+					I am a student and a hobbyist programmer
 				</React.Fragment>
 			</View>
 		);
