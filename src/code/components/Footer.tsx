ae769a847bdaa62f6f6b0a251f92273f35e937db
Šimon Brandner <simon.bra.ag@gmail.com>
2021-08-01 09:22:10 +0200

    Improve footer
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/components/Footer.tsx
+++ src/code/components/Footer.tsx
@@ -5,9 +5,12 @@ export default class App extends React.Component {
 	public render(): JSX.Element {
 		return (
 			<footer className="Footer">
-				<a href="https://github.com/SimonBrandner/personal-website">Source code on GitHub</a>
-				&nbsp;&middot;
-				Uses&nbsp;<a href="https://github.com/google/material-design-icons">Material design icons</a>
+				<div>
+					<a href="https://github.com/SimonBrandner/personal-website">Source code on GitHub</a>
+				</div>
+				<div>
+					Uses <a href="https://github.com/google/material-design-icons">Material design icons</a>
+				</div>
 			</footer>
 		);
 	}
