91fd076b428361fb3118087d4d8dd840c56ea67f
Šimon Brandner <simon.bra.ag@gmail.com>
2022-01-01 20:19:01 +0100

    Make IconButtons accessible
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/components/IconButton.tsx
+++ src/code/components/IconButton.tsx
@@ -1,6 +1,7 @@
 import "../../scss/components/IconButton.scss";
 import React from "react";
 import classNames from "classnames";
+import { Key } from "../Keyboard";
 
 interface IProps {
 	onClick: () => void;
@@ -8,12 +9,18 @@ interface IProps {
 }
 
 export default class Header extends React.Component<IProps> {
+	private onKeyDown = (event: React.KeyboardEvent): void => {
+		if (event.key === Key.Enter) {
+			this.props.onClick();
+		}
+	}
+
 	public render(): JSX.Element {
 		const { onClick, className } = this.props;
 		const classes = classNames("IconButton", className);
 		
 		return (
-			<div className={classes} onClick={onClick} />
+			<div className={classes} onClick={onClick} onKeyDown={this.onKeyDown} tabIndex={0} />
 		);
 	}
 }
