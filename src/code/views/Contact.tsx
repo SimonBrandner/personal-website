b6dd19bd388c9be3d841178649252033871aaa4a
Šimon Brandner <simon.bra.ag@gmail.com>
2022-01-01 21:21:18 +0100

    Migrate to ContactLink
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/views/Contact.tsx
+++ src/code/views/Contact.tsx
@@ -1,84 +1,73 @@
 import "../../scss/views/Contact.scss";
 import React from "react";
 import View from "./View";
+import ContactLink from "../components/ContactLink";
 
 export default class Contact extends React.Component {
 	public render(): JSX.Element {
 		return (
 			<View className="Contact">
 				<React.Fragment>
-					<a 
-						className="Contact_link Contact_link_matrix"
-						href="https://matrix.to/#/@simon.brandner:envs.net"
-					> 
-						Matrix
-					</a>
-					<a 
-						className="Contact_link Contact_link_email"
-						href="mailto:simon.bra.ag@gmail.com"
-					> 
-						Email
-					</a>
-					<a 
-						className="Contact_link Contact_link_github"
-						href="https://github.com/SimonBrandner"
-					> 
-						GitHub
-					</a>
-					<a 
-						className="Contact_link Contact_link_twitter"
-						href="https://twitter.com/BrandnerSimon"
-					> 
-						Twitter
-					</a>
-					<a
-						className="Contact_link Contact_link_youtube"
-						href="https://www.youtube.com/channel/UCe_DknI701POwF5oviZib8g/"
-					>
-						YouTube
-					</a>
-					<a
-						className="Contact_link Contact_link_linkedin"
-						href="https://www.linkedin.com/in/%C5%A1imon-brandner-69a139209/"
-					>
-						LinkedIn
-					</a>
-					<a
-						className="Contact_link Contact_link_reddit"
-						href="https://www.reddit.com/user/SimonBrandner/"
-					>
-						Reddit
-					</a>
-					<a
-						className="Contact_link Contact_link_stackoverflow"
-						href="https://stackoverflow.com/users/10822785/%c5%a0imon-brandner"
-					>
-						Stack Overflow
-					</a>
-					<a
-						className="Contact_link Contact_link_pinterest"
-						href="https://cz.pinterest.com/SimonBrandnerDev"
-					>
-						Pinterest
-					</a>
-					<a
-						className="Contact_link Contact_link_goodreads"
-						href="https://www.goodreads.com/user/show/112089576-imon-brandner"
-					>
-						Goodreads
-					</a>
-					<a
-						className="Contact_link Contact_link_gitlab"
-						href="https://gitlab.com/simon.brandner"
-					>
-						GitLab
-					</a>
-					<a 
-						className="Contact_link Contact_link_location"
-						href="https://www.google.com/maps/place/Czechia/"
-					> 
-						Location
-					</a>
+					<ContactLink 
+						className="matrix"
+						link="https://matrix.to/#/@simon.brandner:envs.net"
+						label="Matrix"
+					/>
+					<ContactLink 
+						className="email"
+						link="mailto:simon.bra.ag@gmail.com"
+						label="Email"
+					/>
+					<ContactLink 
+						className="github"
+						link="https://github.com/SimonBrandner"
+						label="GitHub"
+					/>
+					<ContactLink 
+						className="twitter"
+						link="https://twitter.com/BrandnerSimon"
+						label="Twitter"
+					/>
+					<ContactLink
+						className="youtube"
+						link="https://www.youtube.com/channel/UCe_DknI701POwF5oviZib8g/"
+						label="YouTube"
+					/>
+					<ContactLink
+						className="linkedin"
+						link="https://www.linkedin.com/in/%C5%A1imon-brandner-69a139209/"
+						label="LinkedIn"
+					/>
+					<ContactLink
+						className="reddit"
+						link="https://www.reddit.com/user/SimonBrandner/"
+						label="Reddit"
+					/>
+					<ContactLink
+						className="stackoverflow"
+						link="https://stackoverflow.com/users/10822785/%c5%a0imon-brandner"
+						label="Stack Overflow"
+					/>
+					<ContactLink
+						className="pinterest"
+						link="https://cz.pinterest.com/SimonBrandnerDev"
+						label="Pinterest"
+					/>
+					<ContactLink
+						className="goodreads"
+						link="https://www.goodreads.com/user/show/112089576-imon-brandner"
+						label="Goodreads"
+					/>
+					<ContactLink
+						className="gitlab"
+						link="https://gitlab.com/simon.brandner"
+						label="GitLab"
+					/>
+					<ContactLink 
+						className="location"
+						link="https://www.google.com/maps/place/Czechia/"
+						label="Location"
+					/>
 				</React.Fragment>
 			</View>
 		);
