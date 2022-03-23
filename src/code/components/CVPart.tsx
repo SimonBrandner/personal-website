896d3d2724e3969bedf64366c4dfcdf5be5a3c6d
Šimon Brandner <simon.bra.ag@gmail.com>
2022-03-23 21:21:49 +0100

    Don't require `from`
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/components/CVPart.tsx
+++ src/code/components/CVPart.tsx
@@ -5,20 +5,27 @@ import { parse } from "marked";
 interface IProps {
 	name: string;
 	content: string;
-	from: string;
+	from?: string;
 	to?: string;
 }
 
 export const CVPart: React.FC<IProps> = ({ name, content, from, to }) => {
 	const html = parse(content);
 
+	let fromTo;
+	if (from) {
+		fromTo = ( 
+			<div className="CVPart_title_time">
+				{ from } &nbsp;&ndash;&nbsp; { to || "Present" }
+			</div>
+		);
+	}
+
 	return (
 		<div className="CVPart">
 			<div className="CVPart_title">
 				<div className="CVPart_title_name"> { name } </div>
-				<div className="CVPart_title_time">
-					{ from }&nbsp;&ndash;&nbsp;{ to || "Present" }
-				</div>
+				{ fromTo }
 			</div>
 			<div dangerouslySetInnerHTML={{ __html: html }} />
 		</div>
