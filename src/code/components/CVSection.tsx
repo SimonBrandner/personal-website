dcb6f45a450f4ffa698e75cce4a6dc862f05d86f
Šimon Brandner <simon.bra.ag@gmail.com>
2025-08-31 11:20:53 +0200

    Fix issues
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/code/components/CVSection.tsx
+++ src/code/components/CVSection.tsx
@@ -1,9 +1,9 @@
 import "../../scss/components/CVPart.scss";
-import React from "react";
+import React, { ReactNode } from "react";
 
 interface IProps {
 	name: string;
-	children: Array<JSX.Element> | JSX.Element;
+	children: Array<ReactNode> | ReactNode;
 	last?: boolean;
 }
 
